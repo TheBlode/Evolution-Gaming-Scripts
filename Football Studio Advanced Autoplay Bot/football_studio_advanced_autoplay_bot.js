5375810266e9f86d392303b16825f0134b32a37b
TheBlode <theblode2020@gmail.com>
2021-01-20 05:40:59 +0000

    Update football_studio_advanced_autoplay_bot.js
    - grabbing result from classic mode as more accurate.
    - setting background colour to black.
    - bonus game debug is now coloured on the on-screen debug.
    - balance is shown after each round on the on-screen debug and console debug.
    - winnings are shown after each round on the on-screen debug and console debug.
    -  added getBalance and getWinnings functions.

--- Football Studio Advanced Autoplay Bot/football_studio_advanced_autoplay_bot.js
+++ Football Studio Advanced Autoplay Bot/football_studio_advanced_autoplay_bot.js
@@ -1,69 +1,69 @@
 /* =====================
- * Evolution's Football Studio Advanced Autoplay Bot
- * Written by The_Blode
- * 07/11/20
- * NB! This script must be run in the console of your browser. You must also select the game's iframe by selecting it manually in the Chrome development tools or connecting directly to the iframes URL
- * =====================
- */
+* Evolution's Football Studio Advanced Autoplay Bot
+* Written by The_Blode
+* 07/11/20
+* NB! This script must be run in the console of your browser. You must also select the game's iframe by selecting it manually in the Chrome development tools or connecting directly to the iframes URL
+* =====================
+*/
 
 /* Autoplay mode #1 - Alternate bets. Example: Bet home, then away, then home, then away, then home, then away, etc
- * Autoplay mode #2 - Bet on home and away in numbered sequences. Example: if sequence_amount is set to 3 in a row, the bot will play; home, home, home then away, away, away and repeat.
- * Autoplay mode #3 - Bet randomly on home or away
- * Autoplay mode #4 - Bet only after a certain sequence of results. Example: if streak_size is set to 4, then place a bet after 4 homes or 4 aways in a row
- * Autoplay mode #5 - Bet randomly on home or away (with round skipping)
+* Autoplay mode #2 - Bet on home and away in numbered sequences. Example: if sequence_amount is set to 3 in a row, the bot will play; home, home, home then away, away, away and repeat.
+* Autoplay mode #3 - Bet randomly on home or away
+* Autoplay mode #4 - Bet only after a certain sequence of results. Example: if streak_size is set to 4, then place a bet after 4 homes or 4 aways in a row
+* Autoplay mode #5 - Bet randomly on home or away (with round skipping)
 
- */
+*/
 /* ========================================================================
- * Set autoplay mode and other game settings
- * ======================================================================== */
+* Set autoplay mode and other game settings
+* ======================================================================== */
 var autoplay_mode = 4;
 
 /* ========================================================================
- * Disable video (when you set this to 1, video will be disabled)
- * ======================================================================== */
-var disable_video = 1;
+* Disable video (when you set this to 1, video will be disabled)
+* ======================================================================== */
+var disable_video = 0;
 
 /* ========================================================================
- * Set click delay (if you're having issues with clicks on the UI)
- * ======================================================================== */
+* Set click delay (if you're having issues with clicks on the UI)
+* ======================================================================== */
 var click_delay = 2000;
 
 /* ========================================================================
- * Set wager amount in units (default is 1 unit)
- * ======================================================================== */
+* Set wager amount in units (default is 1 unit)
+* ======================================================================== */
 var user_wager_amount = 1;
 
 /* ========================================================================
- * Set streak size to wait for before betting
- * ======================================================================== */
+* Set streak size to wait for before betting
+* ======================================================================== */
 var user_streak_size = 5;
 
 /* ========================================================================
- * Set sequence amount to play with
- * ======================================================================== */
+* Set sequence amount to play with
+* ======================================================================== */
 var user_sequence_amount = 3;
 
 /* ========================================================================
- * Set round skipping frequency. The higher the number, the more rounds will be skipped.
- * For use of random modes that allow skipping rounds.
- * ======================================================================== */
+* Set round skipping frequency. The higher the number, the more rounds will be skipped.
+* For use of random modes that allow skipping rounds.
+* ======================================================================== */
 var user_round_skipping = 2;
 
 /* ========================================================================
- * Hide UI elements if you want a cleaner interface to play with
- * Hides all UI elements apart from the betting spots (0 will disable this option)
- * ======================================================================== */
+* Hide UI elements if you want a cleaner interface to play with
+* Hides all UI elements apart from the betting spots (0 will disable this option)
+* ======================================================================== */
 var user_clean_interface = 1;
 
 /* ========================================================================
- * Show on screen debug on the game window
- * ======================================================================== */
+* Show on screen debug on the game window
+* ======================================================================== */
 var user_on_screen_debug = 1;
 
 /* =====================
- * End of bot settings
- * =====================
- */
+* End of bot settings
+* =====================
+*/
 // Autoplay types
 // Autoplay mode #1
 // Alternate bets. Example: Bet home, then away, then home, then away, then home, then away, etc
@@ -112,17 +112,19 @@ var away_streak = 0;
 var check = false;
 var count = 0;
 var clicking = "";
+var result = "";
+var result_check_found = "";
 
 /* =====================
- * Functions that will be used by the bot
- * =====================
- */
+* Functions that will be used by the bot
+* =====================
+*/
 /* =====================
- * Function name: autoPlay
- * Function description: this function will perform the autoplaying
- * Date: 07/11/20
- * =====================
- */
+* Function name: autoPlay
+* Function description: this function will perform the autoplaying
+* Date: 07/11/20
+* =====================
+*/
 function autoPlay() {
     // Disable video
     if (disable_video == 1) {
@@ -146,6 +148,7 @@ function autoPlay() {
 
         // Hide winner's chat
         $(".messagesWinnersChat--2UVhf").hide();
+        $(".top-container--33V8c").hide();
     }
 
     // Output debug on game screen if user wants it
@@ -155,7 +158,7 @@ function autoPlay() {
         $div.attr("id", "debug_area");
 
         // CSS
-        $("#debug_area").css({"position": "absolute", "font-size": "x-large", "width": "100%", "height": "98%", "overflow": "overlay", "line-height": "20pt"});
+        $("#debug_area").css({"position": "absolute", "font-size": "x-large", "width": "100%", "height": "98%", "overflow": "overlay", "line-height": "20pt", "background": "black"});
     }
 
     // Debug for the console
@@ -175,13 +178,34 @@ function autoPlay() {
     // Perform main loop
     function f() {
         // Send fake click to keep game alive
-        click(100, 100);
+        click(200, 200);
+
+        // Disable video
+        if (disable_video == 1) {
+            var html = document.getElementsByClassName("transformWrapper--1ywHP")[0].innerHTML = "";
+        }
 
         // Get winning result
-        var result = $(".genericPhrase--1KynV").html();
+        result = $("div[data-role='status-text']").html();
+
+        if (result != undefined) {
+            var result_check = result.match(/WINS/g);
+
+            if (result_check != null) {
+                if (result_check_found == "") {
+                    result_check_found = $("div[data-role='status-text']").html();
+                }
+            }
+        }
+
+        var game_state = $("div[data-role='status-text']").html();
+
+        if (game_state != undefined) {
+            game_state_check = game_state.match(/PLACE/g);
+        }
 
         // Main bot logic
-        if (result != undefined && check == false) {
+        if (game_state_check != null && check == false) {
             // Flip check flag
             check = true;
 
@@ -189,20 +213,66 @@ function autoPlay() {
             iteration_number = i;
 
             // Set counter value
-            count = iteration_number + 10;
+            count = iteration_number + 40;
 
             // Clear interval
             clearInterval(clicking);
 
+            // Format result
+            result_check_found = result_check_found.replace(/ WINS/, "");
+
             // Output final hand to console
             console.log(spacing);
-            console.log("The final result is " + result);
+            console.log("The final result is " + result_check_found);
+            console.log(spacing);
+
+            // Debug for page
+            if (user_on_screen_debug == 1) {
+                if (result_check_found == "Home") {
+                    // Append to debug area
+                    $("#debug_area").append("The final result is <font color=\"red\">" + result_check_found + "</font><br />");
+                } else if (result_check_found == "Away") {
+                    // Append to debug area
+                    $("#debug_area").append("The final result is <font color=\"blue\">" + result_check_found + "</font><br />");
+                } else {
+                    // Append to debug area
+                    $("#debug_area").append("The final result is <font color=\"yellow\">" + result_check_found + "</font><br />");
+                }
+
+                // Scroll to top
+                scrollToTopOfDebug();
+            }
+
+            // Debug for the console
+            console.log(spacing);
+            var winnings = getWinnings();
+            console.log("Your winnings are: " + winnings);
+            console.log(spacing);
+
+            // Debug for page
+            if (user_on_screen_debug == 1) {
+                if (winnings == "0") {
+                // Append to debug area
+                $("#debug_area").append("Your winnings are: <font color=\"red\">" + winnings + "</font><br />");
+                } else {
+                    // Append to debug area
+                    $("#debug_area").append("Your winnings are: <font color=\"green\">" + winnings + "</font><br />");
+                }
+
+                // Scroll to top
+                scrollToTopOfDebug();
+            }
+
+            // Debug for the console
+            console.log(spacing);
+            var balance = getBalance();
+            console.log("Your balance is: " + balance);
             console.log(spacing);
 
             // Debug for page
             if (user_on_screen_debug == 1) {
                 // Append to debug area
-                $("#debug_area").append("The final result is " + result + "<br />");
+                $("#debug_area").append("Your balance is: " + balance + "<br />");
 
                 // Scroll to top
                 scrollToTopOfDebug();
@@ -407,7 +477,7 @@ function autoPlay() {
                     // Debug for page
                     if (user_on_screen_debug == 1) {
                         // Append to debug area
-                        $("#debug_area").append("The randomizer chose home as the next bet!<br />");
+                        $("#debug_area").append("The randomizer chose away as the next bet!<br />");
 
                         // Scroll to top
                         scrollToTopOfDebug();
@@ -542,7 +612,7 @@ function autoPlay() {
             }
 
             // Reset streak
-            if (result == "HOME") {
+            if (result_check_found == "Home") {
                 // Increase home streak
                 home_streak++;
 
@@ -560,7 +630,7 @@ function autoPlay() {
                     // Scroll to top
                     scrollToTopOfDebug();
                 }
-            } else if (result = "AWAY") {
+            } else if (result_check_found = "Away") {
                 // Increase away streak
                 away_streak++;
 
@@ -643,6 +713,9 @@ function autoPlay() {
                     }, click_delay);
                 }
             }
+
+            // Reset result
+            result_check_found = "";
         }
 
         // Check
@@ -663,22 +736,22 @@ function autoPlay() {
 }
 
 /* =====================
- * Function name: randomNumber
- * Function description: this function will generate a random number
- * Date: 07/11/20
- * =====================
- */
+* Function name: randomNumber
+* Function description: this function will generate a random number
+* Date: 07/11/20
+* =====================
+*/
 function randomNumber(min, max) {
     // Minimum and maximum numbers included 
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 /* =====================
- * Function name: click
- * Function description: this function will send a simulated mouse click
- * Date: 07/11/20
- * =====================
- */
+* Function name: click
+* Function description: this function will send a simulated mouse click
+* Date: 07/11/20
+* =====================
+*/
 function click(x, y) {
     var ev = new MouseEvent('click', {
         'view': window,
@@ -694,11 +767,11 @@ function click(x, y) {
 }
 
 /* =====================
- * Function name: checkBetSpot
- * Function description: this function will check if the betting spot is available
- * Date: 07/11/20
- * =====================
- */
+* Function name: checkBetSpot
+* Function description: this function will check if the betting spot is available
+* Date: 07/11/20
+* =====================
+*/
 function checkBetSpot() {
     // Grab betting spot
     var betting_spot = $(".perspective--5cLlU.perspectiveActive--2VeJ7").length;
@@ -712,20 +785,42 @@ function checkBetSpot() {
 }
 
 /* =====================
- * Function name: scrollToTopOfDebug
- * Function description: this function show the freshest line of debug on the onscreen debug
- * Date: 07/11/20
- * =====================
- */
+* Function name: scrollToTopOfDebug
+* Function description: this function show the freshest line of debug on the onscreen debug
+* Date: 07/11/20
+* =====================
+*/
 function scrollToTopOfDebug() {
     // Scroll to top of debug area
     $("#debug_area").scrollTop(1000000);
 }
 
 /* =====================
- * Main code
+ * Function name: getBalance
+ * Function description: this function fetch your balance
+ * Date: 07/11/20
  * =====================
- */
+*/
+function getBalance() {
+    // Grab balance
+    return $("span[data-role='balance-label__value']").html();
+}
+
+/* =====================
+* Function name: getWinnings
+* Function description: this function fetch your winnings
+* Date: 07/11/20
+* =====================
+*/
+function getWinnings() {
+    // Grab balance
+    return $("span[data-role='total-bet-label__value']").html();
+}
+
+/* =====================
+* Main code
+* =====================
+*/
 // Inject jQuery for us to use
 javascript:(function() {
     function l(u, i) {
